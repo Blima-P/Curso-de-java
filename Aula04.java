ab1057ba4c979e01a3b3d97873bae9d6c9f1ade4
Pedro Braga de Lima <p1braga.tech@gmail.com>
2025-10-22 12:01:50 -0300

    aula05

--- Aula04.java
+++ Aula04.java
@@ -1,23 +1,22 @@
 import java.util.Scanner;
 public class Aula04 {
     public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
+        try (Scanner sc = new Scanner(System.in)) {
+            int x;
+            String s1, s2, s3;
 
-        int x;
-        String s1, s2, s3;
+            x = sc.nextInt();
+            sc.nextLine();
+            s1 = sc.nextLine();
+            s2 = sc.nextLine(); 
+            s3 = sc.nextLine();
 
-        x = sc.nextInt();
-        sc.nextLine();
-        s1 = sc.nextLine();
-        s2 = sc.nextLine(); 
-        s3 = sc.nextLine();
-
-        System.out.println("Dados Digitados:");
-        System.out.println(x);
-        System.out.println(s1);
-        System.out.println(s2);
-        System.out.println(s3);
-
+            System.out.println("Dados Digitados:");
+            System.out.println(x);
+            System.out.println(s1);
+            System.out.println(s2);
+            System.out.println(s3);
+        }
     }
 
 }
