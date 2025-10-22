6d510f79b134cf4fc84521241e8a7502ebc04a37
Pedro Braga de Lima <p1braga.tech@gmail.com>
2025-10-22 11:49:25 -0300

    aula04

--- Aula03.java
+++ Aula03.java
@@ -2,21 +2,19 @@ import java.util.Scanner;
 public class Aula03 {
     public static void main(String[] args){
 
-        Scanner sc = new Scanner(System.in);
-        
-        String x;
-        int y;
-        double h;
-
-        x = sc.next();
-        y = sc.nextInt();
-        h = sc.nextDouble();
-
-        System.out.println("Voce digitou:");
-        System.out.println(x);
-        System.out.println(y);
-        System.out.println(h);
-        
-        sc.close();
+        try (Scanner sc = new Scanner(System.in)) {
+            String x;
+            int y;
+            double h;
+            
+            x = sc.next();
+            y = sc.nextInt();
+            h = sc.nextDouble();
+            
+            System.out.println("Voce digitou:");
+            System.out.println(x);
+            System.out.println(y);
+            System.out.println(h);
+        }
     }
 }
