36a1025a1bf53d930be5305cb839e8a83e28746f
Pedro Braga de Lima <p1braga.tech@gmail.com>
2025-10-22 12:46:10 -0300

    Exercicios0203

---
+++ Exercicios2.java
@@ -0,0 +1,6 @@
+public class Exercicios2 {
+    public static void main(String[] args){
+
+        
+    }    
+}
---
+++ Exercicios3.java
@@ -0,0 +1,5 @@
+public class Exercicios3 {
+    public static void main(String[] args) {
+        
+    }
+}
