c73b4d87f720b57abeb2a72be5102cabd0a4ae62
Pedro Braga de Lima <p1braga.tech@gmail.com>
2025-10-22 11:16:34 -0300

    aula02

---
+++ Aula02.java
@@ -0,0 +1,24 @@
+
+public class Aula02 {
+
+    public static void main(String[] args) {
+
+        double b,B,h,area;
+        int x;
+        double y;
+        
+        b = 6.0;
+        B = 8.0;
+        h = 5.0;
+        
+        area = (b + B) / 2.0 * h;
+
+        x = 5;
+        y = 2 * x;
+
+        System.out.println(area);
+        System.out.println(x);
+        System.out.println(y);
+
+    }
+}
